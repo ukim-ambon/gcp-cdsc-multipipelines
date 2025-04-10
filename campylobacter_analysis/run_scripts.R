51ebe170faded83864a072cc77a4fb719d333e4f
qillbel <qillbel@gmail.com>
2025-04-10 19:07:54 +0100

    update with save it back to GCS

--- campylobacter_analysis/run_scripts.R
+++ campylobacter_analysis/run_scripts.R
@@ -1,20 +1,28 @@
 library(bigrquery)
 
-# Authenticate using the service account key
+# Authenticate BigQuery
 bq_auth(path = "/app/key.json")
+# Authenticate GCS
+cs_auth(json_file = "/app/key.json")
 
-# Set project ID
+# Set variables
 project_id <- "cdsc-projects"
+bucket_name <- "cdsc_pipelines"
+output_path <- "campylobacter_analysis/outputs/tab_OpenNames.csv"
 
 # Define the SQL query
 query <- "SELECT * FROM `dashgcp-452719.qb_ordsur_1_dset.tab_OpenNames` LIMIT 10"
-
-# Execute the query
 result <- bq_project_query(project_id, query)
-
-# Convert to dataframe
 df <- bq_table_download(result)
 
-# Print the results
-print("show dataframe...")
-print(df)
+write.csv(df, "/tmp/tab_OpenNames.csv", row.names = FALSE)
+
+# Upload to GCS
+gcs_upload(
+  file = "/tmp/tab_OpenNames.csv",
+  bucket = bucket_name,
+  name = output_path,
+  predefinedAcl = "bucketLevel"
+)
+print("CSV uploaded to GCS successfully!")
+print("Can be downloaded: https://storage.googleapis.com/cdsc_pipelines/campylobacter_analysis/outputs/tab_OpenNames.csv")
