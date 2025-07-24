860c97fcc55d6d348fe7b500cc0fe6956127a2b9
qillbel <qillbel@gmail.com>
2025-07-24 11:08:36 +0100

    repeat pr

--- campylobacter_analysis/tests/uTest_start.R
+++ campylobacter_analysis/tests/uTest_start.R
@@ -4,4 +4,4 @@ results <- test_dir("tests/testthat", reporter = "summary")
 if (any(vapply(results, function(x) x$failed > 0 || x$error, logical(1)))) {
   quit(status = 1)
 }
-cat("\n All unit tests passed!\n")
+cat("\n All unit tests have passed!\n")
