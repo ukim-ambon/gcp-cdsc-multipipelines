b661ea9cc1fee17551f832b3ae2b90137a615af6
qillbel <qillbel@gmail.com>
2025-07-30 09:10:29 +0100

    execute and test2

--- campylobacter_analysis/tests/testthat/test-uTest1.R
+++ campylobacter_analysis/tests/testthat/test-uTest1.R
@@ -1,4 +1,4 @@
-source("../../run_script1.R")
+source("run_script1.R")
 
 test_that("uTest1 returns correct message", {
   expect_equal(uTest1(), "Hello Campylobacter_analysis pipeline with R script 1d")
